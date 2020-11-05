349419c6437f805bce096838d257b0e06e19aff3
jaggerw <jaggerw12138@163.com>
2020-11-05 16:26:35 +0800

    验证邮箱格式

---
+++ src/main/java/com/nju/banxing/demo/annotation/IsEmail.java
@@ -0,0 +1,29 @@
+package com.nju.banxing.demo.annotation;
+
+import com.nju.banxing.demo.validator.IsMobileValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * @Author: jaggerw
+ * @Description: 验证邮箱注解
+ * @Date: 2020/11/5
+ */
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(
+        validatedBy = {IsMobileValidator.class}
+)
+public @interface IsEmail {
+    // email是否可以为空，默认是不能为空
+    boolean required() default true;
+
+    String message() default "邮箱格式错误!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
--- src/main/java/com/nju/banxing/demo/util/ValidatorUtil.java
+++ src/main/java/com/nju/banxing/demo/util/ValidatorUtil.java
@@ -13,6 +13,8 @@ import java.util.regex.Pattern;
 public class ValidatorUtil {
     private static final Pattern mobilePattern = Pattern.compile("1[35678]\\d{9}");
 
+    private static final Pattern emailPattern = Pattern.compile("^([A-Za-z0-9_\\-.])+@([A-Za-z0-9_\\-.])+\\.([A-Za-z]{2,4})$");
+
     /**
      * 判断是否符合手机号格式
      *
@@ -26,4 +28,13 @@ public class ValidatorUtil {
         Matcher m = mobilePattern.matcher(inputStr);
         return m.matches();
     }
+
+    public static boolean isEmail(String inputStr){
+        if (StringUtils.isEmpty(inputStr)) {
+            return false;
+        }
+        Matcher m = emailPattern.matcher(inputStr);
+        return m.matches();
+    }
+
 }
