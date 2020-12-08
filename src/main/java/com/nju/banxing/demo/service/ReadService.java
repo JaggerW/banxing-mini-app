37c3f6b20ee365f580b5bfa93a5ea38294269600
jaggerw <jaggerw12138@163.com>
2020-12-09 00:08:42 +0800

    debug

--- src/main/java/com/nju/banxing/demo/service/ReadService.java
+++ src/main/java/com/nju/banxing/demo/service/ReadService.java
@@ -3,12 +3,14 @@ package com.nju.banxing.demo.service;
 import com.nju.banxing.demo.domain.ReadDO;
 import com.nju.banxing.demo.domain.mapper.ReadMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author: jaggerw
  * @Description: 用户已读时间
  * @Date: 2020/12/8
  */
+@Service
 public class ReadService {
 
     @Autowired
