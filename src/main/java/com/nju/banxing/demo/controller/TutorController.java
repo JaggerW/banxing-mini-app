9568d1b2c58eb175026c3d8e603b7ca17714ed81
jaggerw <jaggerw12138@163.com>
2020-12-29 21:35:41 +0800

    todo_list

--- src/main/java/com/nju/banxing/demo/controller/TutorController.java
+++ src/main/java/com/nju/banxing/demo/controller/TutorController.java
@@ -30,7 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import sun.security.action.GetLongAction;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
